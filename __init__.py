d9d7ab88201935c91db90e43d14342280c9612b4
Grigory-Yunusov <grigoriyzawr@gmail.com>
2023-10-24 00:22:26 +0300

    second_commit

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from foo import foo
+from baz.operation import mul, sum
+from bar.info import log
---
+++ main.py
@@ -0,0 +1,8 @@
+from foo import foo
+from baz.operation import mul, sum
+from bar.info import log
+
+print(foo("Selime"))
+print(sum(1, 4))
+print(mul(5, 6))
+log("Hello piple")
